3dbd196b03292abc94f5405c52caa203c6b6be41
saitej25 <31233091+saitej25@users.noreply.github.com>
2018-11-27 20:09:02 -0800

    Update test.sh

--- simulator/test.sh
+++ simulator/test.sh
@@ -1,2 +1,2 @@
 export HOME_DIR=$PWD
-./Run.pl -db ./bench.db -dir results/gcc1 -benchmark gcc -sim $HOME_DIR/ss3/sim-outorder -args "-max:inst 1000000"
+./Run.pl -db ./bench.db -dir results/gcc1 -benchmark gcc -sim $HOME_DIR/ss3/sim-outorder -args "-bpred perceptron -max:inst 1000000"
